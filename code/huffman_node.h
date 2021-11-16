eaceaa844d1b403c081b4a66e52d91f71574a113
liuly0322 <liuly0322@mail.ustc.edu.cn>
2021-11-16 21:41:29 +0800

    增加由子节点数组的huffman结点构造函数

--- code/huffman_node.h
+++ code/huffman_node.h
@@ -14,6 +14,16 @@ typedef struct huffman_node {
         weight = 0;
         _data = new bits(size, data);
     }
+	huffman_node(int n, huffman_node** nodes) {
+		_data = nullptr;
+		parent = nullptr;
+		weight = 0;
+		children = new huffman_node*[n]();
+		for (int i = 0; i < n; i++) {
+			weight += nodes[i]->weight;
+			children[i] = nodes[i];
+		}
+	}
 } h_node;
 
 #endif
